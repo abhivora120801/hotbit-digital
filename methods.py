d49dbbc6e7a36d0ddbda2c9dcbff77e8d5c0eb89
Abhi Vora <abhivora120801@gmail.com>
2024-04-16 18:01:25 +0530

    added automate prospects

--- methods.py
+++ methods.py
@@ -1,5 +1,6 @@
 from distutils.command import upload
 from fileinput import filename
+from random import randint
 from xml import dom
 import boto3
 from selenium import webdriver
@@ -30,25 +31,10 @@ def login_apollo(driver,user):
     driver.find_element(By.NAME, 'password').send_keys(password)
     submit_btn = driver.find_element(By.CSS_SELECTOR,"[type = 'submit']").click()
 
-def login_semrush(driver,user):
-    username, password = get_sales_navigator_creds(user)
-    driver.get("https://proseotools.us/user/login.php")
-    time.sleep(2)
-
-    driver.find_element(By.ID,'inputEmail').send_keys(username)
-    driver.find_element(By.ID,'inputPassword').send_keys(password)
-    driver.find_element(By.XPATH,'/html/body/div[2]/form/button').click()
-    #add driver delay
-    time.sleep(15)
-    driver.get('https://proseotools.us/user/in/access_semrush.php')
-
 def init_s3():
     id,key = get_s3_creds()
     s3 = boto3.client('s3', region_name='us-east-1',aws_access_key_id=id,aws_secret_access_key=key)
     return s3
-
-def go_to_lists(driver):
-    driver.get("https://app.apollo.io/#/people/tags?teamListsOnly[]=no")
 
 def get_page_count(driver):
     page_count = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[2]/div/div[4]/div/div/div/div/div[3]/div/div[1]/span").text
@@ -64,7 +50,7 @@ def sort_by_name(driver):
     url = driver.current_url+"&sortByField=person_name.raw&sortAscending=true"
     driver.get(url)
 
-def extract_data(row,bucket_name,competitor_domain,competitor_name):
+def extract_data(row,):
     com_linked_in=''
     linked_in=''
     name = row.findAll('td')[0].text.split(' ')[0]
@@ -126,7 +112,7 @@ def get_data(driver,page_count,competitor_domain,competitor_name,industry,countr
         the_soup = BeautifulSoup(driver.page_source, 'html.parser')
         table=the_soup.findAll('tbody')
         for row in table:
-            data.append(extract_data(row,bucket_name,competitor_domain,competitor_name))
+            data.append(extract_data(row))
         if i<page_count:
             driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div[2]/div/div[3]/div/div/div/div/div[3]/div/div[2]/button[2]").click()
     return data,bucket_name,csv_filename
@@ -148,64 +134,45 @@ def write_to_csv(data,filename,bucket_name):
     s3.upload_file("/tmp/"+filename,bucket_name,filename,ExtraArgs={'ACL':'public-read'})
     return f"https://{bucket_name}.s3.amazonaws.com/{filename}"
 
-def screenshot_to_s3(s3,driver,bucket_name,domain,region='us'):
-    domain = domain
-    driver.get(f"https://sm.proseotools.us/analytics/overview/?searchType=domain&q={domain}&db={region}")
-    time.sleep(5)
+def automate_prospecting(driver, url, list_name,num):
+    driver.get(url)
+    time.sleep(randint(5, 10))
 
-    #highlight borders
-    driver.execute_script("arguments[0].style.border='3px solid red'", driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[1]/section/div/div[2]/div[1]/h1/div[2]'))
-    driver.execute_script("arguments[0].style.border='3px solid red'", driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[2]/div/section[1]/div/div/div[1]'))
-    driver.execute_script("arguments[0].style.border='3px solid red'", driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[2]/div/section[1]/div/div/div[2]'))
-    driver.execute_script("arguments[0].style.border='3px solid red'", driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[2]/div/section[1]/div/div/div[3]'))
-    driver.execute_script("arguments[0].style.border='3px solid red'", driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[2]/div/section[1]/div/div/div[4]'))
+    for i in range(num):
+
+        #click on select all
+        driver.find_element(By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div[2]/div/div[3]/div/div/div/div[1]/div[1]/div/button').click()
+        time.sleep(randint(5, 10))
+
+        #select all
+        driver.find_element(By.XPATH,'/html/body/div[7]/div/div/div/div/div/a[1]').click()
+        time.sleep(randint(5, 10))
+
+        # #open add to list
+        # driver.find_element(By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[2]/div/div[3]/div/div/div/div[1]/div[4]/button[3]').click()
+        # time.sleep(randint(5, 10))
+
+        # #click on add to list
+        # driver.find_element(By.XPATH,'/html/body/div[7]/div/div/div/div/div/a').click()
+        # time.sleep(randint(5, 10))
+
+        # click on save button
+        driver.find_element(By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div/div[2]/div[2]/div/div/div/div/div/div/div/div[2]/div/div[3]/div/div/div/div[1]/div[3]/span[1]/button').click()
+        time.sleep(randint(5, 10))
+
+        #send list name
+        driver.find_element(By.XPATH,'/html/body/div[7]/div[2]/div/div/div[2]/form/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div/div[1]/input').send_keys(list_name)
+        time.sleep(randint(5, 10))
+
+        #click on save button
+        driver.find_element(By.XPATH,'/html/body/div[7]/div[2]/div/div/div[3]/button[1]').click()
+        time.sleep(randint(5, 10))
+
+        #refresh prospects
+        driver.find_element(By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div/div[4]/div/div[2]/div/div/div/div[2]/div/div[2]/div[2]').click()
+        time.sleep(randint(5, 10))
 
 
-    time.sleep(1)    # take screenshot
-    screenshot1 = driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[1]').screenshot_as_png
-    screenshot2 = driver.find_element(By.XPATH,'/html/body/div[2]/main/div/div/div[2]/div/section[1]').screenshot_as_png
-
-        # add both images in vertical order
-    img1 = Image.open(BytesIO(screenshot1))
-    img2 = Image.open(BytesIO(screenshot2))
-
-    # resize images to same width
-    img1 = img1.resize((img2.width, img1.height))
-    img2 = img2.resize((img1.width, img2.height))
-
-    # create a new image with the same size
-    new_img = Image.new('RGB', (img1.width, img1.height + img2.height))
-    new_img.paste(img1, (0, 0))
-    new_img.paste(img2, (0, img1.height))
-
-    # upload image to s3
-    object_name = f"{domain}.png"
-    new_img.save("/tmp/"+object_name)
-    s3.upload_file("/tmp/"+object_name, bucket_name, object_name,ExtraArgs={'ACL': 'public-read'})
-
-    return True
-
-def update_screenshots_to_s3(s3,driver,bucket_name,data,region='us'):
-    for item in data:
-        domain = item['company_domain']
-        screenshot_to_s3(s3=s3,driver=driver,bucket_name=bucket_name,domain=domain,region=region)
-        
-    return True
-
-def update_competitor_details(data,competitor_name,competitor_domain,bucket_name):
-    for i in range(len(data)):
-        data[i]['competitor_name']=competitor_name
-        data[i]['competitor_semrush']=f"https://real-estate-ny-us-20-02-2024.s3.amazonaws.com/{competitor_domain}.png"
-    return data
-
-def update_competitor_and_company_details(data,competitor_name,competitor_domain,bucket_name):
-    for i in range(len(data)):
-        data[i]['competitor_name']=competitor_name
-        data[i]['competitor_semrush']=f"https://{bucket_name}.s3.amazonaws.com/{competitor_domain}.png"
-        data[i]['company_semrush']=f"https://{bucket_name}.s3.amazonaws.com/{data[i]['company_domain']}.png"
-    return data
-
-def read_from_csv(filename):
-    with open(filename) as file:
-        data = file.read()
-    return data
+        # # next page
+        # driver.find_element(By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div[2]/div/div[4]/div/div/div/div/div[3]/div/div[2]/button[2]').click()
+        # time.sleep(randint(5, 10))
